69825a35ba14d342718609762de70f1e47f92346
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2024-09-23 14:46:12 +0200

    jo

--- src/main/java/schuli/recursion/Basic.java
+++ src/main/java/schuli/recursion/Basic.java
@@ -3,8 +3,16 @@ package schuli.recursion;
 public class Basic {
     public static void main( String[] args ) {
 
-        System.out.println( fibonacci( 8 ) );
+        System.out.println( mult( 3, 7 ) );
 
+    }
+
+    public static int faculty( int n ) {
+        if ( n > 1 ) {
+            return 1;
+        }
+
+        return n * faculty( n - 1 );
     }
 
     public static int fibonacci( int n ) {
@@ -15,5 +23,13 @@ public class Basic {
         }
 
         return fibonacci( n - 2 ) + fibonacci( n - 1 );
+    }
+
+    public static int mult( int a, int b ) {
+        if ( a == 0 ) {
+            return 0;
+        }
+
+        return b + mult( a - 1, b );
     }
 }
