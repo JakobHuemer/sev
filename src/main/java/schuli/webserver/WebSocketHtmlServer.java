39b4a72a02de0bca45200d2ed4a14fb1ae190aeb
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-03-18 15:53:15 +0100

    feat: webserver reads request body

--- src/main/java/schuli/webserver/WebSocketHtmlServer.java
+++ src/main/java/schuli/webserver/WebSocketHtmlServer.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class WebSocketHtmlServer {
 
@@ -27,9 +28,53 @@ public class WebSocketHtmlServer {
         try ( client ) {
 
 
+            //GET / HTTP/1.1
+            //Host: localhost:8080
+            //User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:136.0) Gecko/20100101 Firefox/136.0
+            //Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+            //Accept-Language: en-GB
+            //Accept-Encoding: gzip, deflate, br, zstd
+            //DNT: 1
+            //Connection: keep-alive
+            //Cookie: pmaUser-1=91HApubZsTWQb%2FWwlI3PF4ZQE7OLXIEAHGMw2mF1TgiOdjTcZ%2FMZEELcyT8%3D; pma_lang=en_GB; pga4_session=9d3a279c-555a-4331-9b2c-3b764466bb8e!TLRKYug9LuKaQ1e859lZK6d6JgifCTvb+tQO+VVkW40=; PHPSESSID=2a126dfff002cf8754b21184f352456c; phpMyAdmin=c0a3b80a881d17e76a27472a07cde8e1; PGADMIN_LANGUAGE=en; pmaAuth-1=7%2BsHU8Y3P%2Bje6No2qqIs7gujzxjO556gFVIdfhYVrteqF36yLVU6QvhtA%2Fwu6Wvj2KbKvUnXVevAed6Jx290lk1p4Q%3D%3D
+            //Upgrade-Insecure-Requests: 1
+            //Sec-Fetch-Dest: document
+            //Sec-Fetch-Mode: navigate
+            //Sec-Fetch-Site: cross-site
+            //Priority: u=0, i
+
+
             var outputStream = client.getOutputStream();
+            var sc = new Scanner( client.getInputStream() );
+
+
+            while ( sc.hasNextLine() ) {
+                var currLine = sc.nextLine();
+                System.out.println( '"' + currLine + '"' );
+
+                if ( currLine.startsWith( "GET" ) ) {
+                    var parts = currLine.split( " " );
+                    if ( parts.length != 3 ) {
+                        throw new RuntimeException( "Invalid Request" );
+                    }
+
+                    if ( !parts[ 1 ].equals( "/" ) ) {
+                        throw new RuntimeException( "Invalid Request" );
+                    }
+                }
+
+                if ( currLine.isBlank() ) {
+                    System.out.println( "End of Protocol" );
+                    break;
+                }
+            }
+
+
+            System.out.println( "Done Getting REQUEST" );
 
             try ( InputStream is = new FileInputStream( HTTP_FILE_PATH ) ) {
+
+                System.out.println( "SENDING DATA" );
                 outputStream.write( is.readAllBytes() );
             } catch ( IOException e ) {
                 throw new RuntimeException( "Failed to Open index.html" );
